955edb654caa34b476fa1fbc3c51f7d503365fc4
the.count@s-aws.com <the.count@s-aws.com>
2026-02-27 16:06:15 -0500

    config change

--- configuration.py
+++ configuration.py
@@ -125,8 +125,8 @@ class OrderBook():
             "SELL": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 2
         },
         "FUTURE": { # does not include 0.15 per contract fee so we use the highest fee for calc
-            "BUY": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 77,
-            "SELL": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 77
+            "BUY": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 7,
+            "SELL": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 7
         }
     }
 
