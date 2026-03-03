57681997d5d19885f6caa23472fd22047018fed2
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-03 08:01:58 -0500

    config change

--- order.py
+++ order.py
@@ -120,17 +120,17 @@ if __name__ == "__main__":
     # )
 
     # BTC
-    # orders = create_limit_order_span(
-    #     delay_in_secs=0,
-    #     product_id="BIP-20DEC30-CDE",
-    #     side="SELL",
-    #     order_base_size=2,
-    #     order_price_difference=25,
-    #     start_price=67700,
-    #     max_order_count=100
-    # )
-    # print(f"Count: {len(orders)}")
-    # print(orders[-1])
+    orders = create_limit_order_span( #size=2, diff=25, count=300, 
+        delay_in_secs=0,
+        product_id="BIP-20DEC30-CDE",
+        side="SELL",
+        order_base_size=2,
+        order_price_difference=25,
+        start_price=67700,
+        max_order_count=100
+    )
+    print(f"Count: {len(orders)}")
+    print(orders[-1])
 
     orders = create_limit_order_span( #size=2, diff=25, count=300, 
         delay_in_secs=0,
