899924bb8cb3464db35819e74d089d22f0125dcb
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-05 11:25:59 -0500

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
         product_id="BIP-20DEC30-CDE",
         side="SELL",
         order_base_size=20,
-        order_price_difference=200,
+        order_price_difference=300,
         start_price=71200,
-        max_order_count=20
+        max_order_count=10
     )
     print(f"Count: {len(orders)}")
     print(orders[-1])
