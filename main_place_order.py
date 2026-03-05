85682a46fa83a6401e89432dd8b96a2316703ac4
the.count@s-aws.com <the.count@s-aws.com>
2026-03-05 03:03:15 -0500

    config change

--- main_place_order.py
+++ main_place_order.py
@@ -4,23 +4,23 @@ from order import create_limit_order_span
 if __name__ == "__main__":
     ##################### PERP
     # BTC
-    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
-    #     delay_in_secs=0,
-    #     product_id="BIP-20DEC30-CDE",
-    #     side="SELL",
-    #     order_base_size=25,
-    #     order_price_difference=1000,
-    #     start_price=71400,
-    #     max_order_count=10
-    # )
-    # print(f"Count: {len(orders)}")
-    # print(orders[-1])
+    orders = create_limit_order_span( #size=2, diff=25, count=100, 
+        delay_in_secs=0,
+        product_id="BIP-20DEC30-CDE",
+        side="SELL",
+        order_base_size=5,
+        order_price_difference=100,
+        start_price=72200,
+        max_order_count=20
+    )
+    print(f"Count: {len(orders)}")
+    print(orders[-1])
 
     # orders = create_limit_order_span( #size=2, diff=25, count=100, 
     #     delay_in_secs=0,
     #     product_id="BIP-20DEC30-CDE",
     #     side="BUY",
-    #     order_base_size=20,
+    #     order_base_size=5,
     #     order_price_difference=100,
     #     start_price=72600,
     #     max_order_count=20
@@ -30,16 +30,16 @@ if __name__ == "__main__":
 
     ##################### SPOT
 
-    orders = create_limit_order_span(
-        delay_in_secs=0,
-        product_id="BTC-USDC",
-        side="BUY",
-        order_base_size=0.001,
-        order_price_difference=20,
-        start_price=72800,
-        max_order_count=100,
-        post_only=True
-    )
+    # orders = create_limit_order_span(
+    #     delay_in_secs=0,
+    #     product_id="BTC-USDC",
+    #     side="BUY",
+    #     order_base_size=0.001,
+    #     order_price_difference=20,
+    #     start_price=72800,
+    #     max_order_count=100,
+    #     post_only=True
+    # )
 
-    print(f"Count: {len(orders)}")
-    print(orders[-1])
+    # print(f"Count: {len(orders)}")
+    # print(orders[-1])
