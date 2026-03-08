494802847debc1facac5e36d12838bf90db20b97
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-08 18:26:12 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         product_id="BIP-20DEC30-CDE",
         side="SELL",
         order_base_size=20,
-        order_price_difference=150,
+        order_price_difference=250,
         start_price=67000,
         max_order_count=10
     )
