24e401f2d36324e1959670cf55681bd24eaa52a7
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-09 12:49:39 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
         delay_in_secs=0,
         product_id="BIP-20DEC30-CDE",
         side="SELL",
-        order_base_size_range={"start": 1, "stop": 2},
-        order_price_difference=125,
-        start_price=69610,
-        max_order_count=50
+        order_base_size_range={"start": 1, "stop": 5},
+        order_price_difference=250,
+        start_price=68700,
+        max_order_count=4
     )
     print(f"Count: {len(orders)}")
     print(orders[-1])
