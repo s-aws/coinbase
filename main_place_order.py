f209550c2c0301945cbb2b0c0810e8dadd6a2a73
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-11 21:59:24 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -8,25 +8,25 @@ if __name__ == "__main__":
         delay_in_secs=0,
         product_id="BIP-20DEC30-CDE",
         side="SELL",
-        order_base_size_range={"start": 1, "stop": 5},
-        order_price_difference=75,
-        start_price=71000,
+        order_base_size_range={"start": 5, "stop": 10},
+        order_price_difference=115,
+        start_price=70065,
         max_order_count=20
     )
     print(f"Count: {len(orders)}")
     print(orders[-1])
 
-    orders = create_limit_order_span( #size=2, diff=25, count=100, 
-        delay_in_secs=0,
-        product_id="BIP-20DEC30-CDE",
-        side="BUY",
-        order_base_size_range={"start": 1, "stop": 5},
-        order_price_difference=75,
-        start_price=70000,
-        max_order_count=20
-    )
-    print(f"Count: {len(orders)}")
-    print(orders[-1])
+    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
+    #     delay_in_secs=0,
+    #     product_id="BIP-20DEC30-CDE",
+    #     side="BUY",
+    #     order_base_size_range={"start": 1, "stop": 5},
+    #     order_price_difference=75,
+    #     start_price=70000,
+    #     max_order_count=20
+    # )
+    # print(f"Count: {len(orders)}")
+    # print(orders[-1])
 
 
     ##################### SPOT
