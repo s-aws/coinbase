de656aa8ad8c8851397b1cf3788f4811494026ae
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-12 18:18:15 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -4,17 +4,17 @@ from order import create_limit_order_span
 if __name__ == "__main__":
     ##################### PERP
     # BTC
-    orders = create_limit_order_span( #size=2, diff=25, count=100, 
-        delay_in_secs=0,
-        product_id="BIP-20DEC30-CDE",
-        side="SELL",
-        order_base_size_range={"start": 1, "stop": 2},
-        order_price_difference=100,
-        start_price=70700,
-        max_order_count=50
-    )
-    print(f"Count: {len(orders)}")
-    print(orders[-1])
+    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
+    #     delay_in_secs=0,
+    #     product_id="BIP-20DEC30-CDE",
+    #     side="SELL",
+    #     order_base_size_range={"start": 1, "stop": 2},
+    #     order_price_difference=100,
+    #     start_price=70700,
+    #     max_order_count=50
+    # )
+    # print(f"Count: {len(orders)}")
+    # print(orders[-1])
 
     orders = create_limit_order_span( #size=2, diff=25, count=100, 
         delay_in_secs=0,
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         side="BUY",
         order_base_size_range={"start": 1, "stop": 2},
         order_price_difference=50,
-        start_price=70300,
+        start_price=68000,
         max_order_count=20
     )
     print(f"Count: {len(orders)}")
