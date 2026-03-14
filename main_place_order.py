5f88e7b3d22b2bdcc9bc887083a5477c461181d0
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-14 06:40:45 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -8,22 +8,23 @@ if __name__ == "__main__":
         delay_in_secs=0,
         product_id="BIP-20DEC30-CDE",
         side="SELL",
-        order_base_size_range={"start": 1, "stop": 2},
+        order_base_size_range={"start": 5, "stop": 10},
         order_price_difference=250,
-        start_price=73200,
-        max_order_count=20
+        start_price=71455,
+        max_order_count=10
     )
     print(f"Count: {len(orders)}")
     print(orders[-1])
+
 
     # orders = create_limit_order_span( #size=2, diff=25, count=100, 
     #     delay_in_secs=0,
     #     product_id="BIP-20DEC30-CDE",
     #     side="BUY",
-    #     order_base_size_range={"start": 1, "stop": 2},
+    #     order_base_size_range={"start": 5, "stop": 10},
     #     order_price_difference=250,
-    #     start_price=72050,
-    #     max_order_count=17
+    #     start_price=70500,
+    #     max_order_count=10
     # )
     # print(f"Count: {len(orders)}")
     # print(orders[-1])
@@ -34,11 +35,11 @@ if __name__ == "__main__":
     # orders = create_limit_order_span(
     #     delay_in_secs=0,
     #     product_id="BTC-USDC",
-    #     side="BUY",
-    #     order_base_size=0.001,
-    #     order_price_difference=100,
-    #     start_price=68022,
-    #     max_order_count=1,
+    #     side="SELL",
+    #     order_base_size_range={"start": 0.0001, "stop": 0.001},
+    #     order_price_difference=21.01,
+    #     start_price=71050.51,
+    #     max_order_count=100,
     #     post_only=True
     # )
 
