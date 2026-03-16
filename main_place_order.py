2082787ce636255562c3e68a2ebbaf7fa7167801
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-16 16:33:45 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -4,29 +4,29 @@ from order import create_limit_order_span
 if __name__ == "__main__":
     ##################### PERP
     # BTC
+    orders = create_limit_order_span( #size=2, diff=25, count=100, 
+        delay_in_secs=0,
+        product_id="BIP-20DEC30-CDE",
+        side="SELL",
+        order_base_size_range={"start": 1, "stop": 2},
+        order_price_difference=100,
+        start_price=74100,
+        max_order_count=43
+    )
+    print(f"Count: {len(orders)}")
+    print(orders[-1])
+
     # orders = create_limit_order_span( #size=2, diff=25, count=100, 
     #     delay_in_secs=0,
     #     product_id="BIP-20DEC30-CDE",
-    #     side="SELL",
+    #     side="BUY",
     #     order_base_size_range={"start": 1, "stop": 2},
     #     order_price_difference=100,
-    #     start_price=73900,
+    #     start_price=73000,
     #     max_order_count=100
     # )
     # print(f"Count: {len(orders)}")
     # print(orders[-1])
-
-    orders = create_limit_order_span( #size=2, diff=25, count=100, 
-        delay_in_secs=0,
-        product_id="BIP-20DEC30-CDE",
-        side="BUY",
-        order_base_size_range={"start": 1, "stop": 2},
-        order_price_difference=100,
-        start_price=73600,
-        max_order_count=39
-    )
-    print(f"Count: {len(orders)}")
-    print(orders[-1])
 
 
     ##################### SPOT
