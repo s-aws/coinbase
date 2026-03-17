6bea0a30905794c49d6e86bcc098a4289a50b2ae
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-17 08:19:21 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -10,23 +10,23 @@ if __name__ == "__main__":
         side="SELL",
         order_base_size_range={"start": 1, "stop": 2},
         order_price_difference=100,
-        start_price=74100,
-        max_order_count=43
+        start_price=75000,
+        max_order_count=33
     )
     print(f"Count: {len(orders)}")
     print(orders[-1])
 
-    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
-    #     delay_in_secs=0,
-    #     product_id="BIP-20DEC30-CDE",
-    #     side="BUY",
-    #     order_base_size_range={"start": 1, "stop": 2},
-    #     order_price_difference=100,
-    #     start_price=73000,
-    #     max_order_count=100
-    # )
-    # print(f"Count: {len(orders)}")
-    # print(orders[-1])
+    orders = create_limit_order_span( #size=2, diff=25, count=100, 
+        delay_in_secs=0,
+        product_id="BIP-20DEC30-CDE",
+        side="BUY",
+        order_base_size_range={"start": 1, "stop": 2},
+        order_price_difference=30,
+        start_price=73500,
+        max_order_count=50
+    )
+    print(f"Count: {len(orders)}")
+    print(orders[-1])
 
 
     ##################### SPOT
