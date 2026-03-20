751d296d5b3543a275e54e974efd524b513ec3ac
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-20 11:00:28 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -8,27 +8,27 @@ if __name__ == "__main__":
         delay_in_secs=0,
         product_id="BIP-20DEC30-CDE",
         side="SELL",
-        order_base_size_range={"start": 2, "stop": 10},
-        order_price_difference=75,
-        start_price=70350,
+        order_base_size_range={"start": 5, "stop": 20},
+        order_price_difference=10,
+        start_price=70000,
         max_order_count=10,
         post_only=True
     )
     print(f"Count: {len(orders)}")
     print(orders[-1])
 
-    orders = create_limit_order_span( #size=2, diff=25, count=100, 
-        delay_in_secs=0,
-        product_id="BIP-20DEC30-CDE",
-        side="BUY",
-        order_base_size_range={"start": 2, "stop": 5},
-        order_price_difference=75,
-        start_price=70200,
-        max_order_count=20,
-        post_only=True
-    )
-    print(f"Count: {len(orders)}")
-    print(orders[-1])
+    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
+    #     delay_in_secs=0,
+    #     product_id="BIP-20DEC30-CDE",
+    #     side="BUY",
+    #     order_base_size_range={"start": 2, "stop": 5},
+    #     order_price_difference=75,
+    #     start_price=70200,
+    #     max_order_count=20,
+    #     post_only=True
+    # )
+    # print(f"Count: {len(orders)}")
+    # print(orders[-1])
 
 
     ##################### SPOT
