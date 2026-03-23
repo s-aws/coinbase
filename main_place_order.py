5b69860cac99e9223d2f3b760441ff27a6e27a28
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-23 12:11:18 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         side="SELL",
         order_base_size_range={"start": 10, "stop": 20},
         order_price_difference=200,
-        start_price=71200,
+        start_price=70800,
         max_order_count=10,
         post_only=True
     )
