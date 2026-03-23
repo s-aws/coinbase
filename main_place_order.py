c2b3a6e56f31c09298d3160baa3a04008b0b15b6
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-22 21:48:12 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -17,32 +17,32 @@ if __name__ == "__main__":
     # print(f"Count: {len(orders)}")
     # print(orders[-1])
 
-    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
-    #     delay_in_secs=0,
-    #     product_id="BIP-20DEC30-CDE",
-    #     side="BUY",
-    #     order_base_size_range={"start": 10, "stop": 10},
-    #     order_price_difference=100,
-    #     start_price=67600,
-    #     max_order_count=20,
-    #     post_only=True
-    # )
-    # print(f"Count: {len(orders)}")
-    # print(orders[-1])
-
-    # PAXG
-    orders = create_limit_order_span(
+    orders = create_limit_order_span( #size=2, diff=25, count=100, 
         delay_in_secs=0,
-        product_id="PAU-20DEC30-CDE",
+        product_id="BIP-20DEC30-CDE",
         side="BUY",
-        order_base_size_range={"start": 1, "stop": 2},
-        order_price_difference=25,
-        start_price=4500,
+        order_base_size_range={"start": 10, "stop": 20},
+        order_price_difference=100,
+        start_price=67600,
         max_order_count=20,
         post_only=True
     )
     print(f"Count: {len(orders)}")
     print(orders[-1])
+
+    # PAXG
+    # orders = create_limit_order_span(
+    #     delay_in_secs=0,
+    #     product_id="PAU-20DEC30-CDE",
+    #     side="BUY",
+    #     order_base_size_range={"start": 1, "stop": 2},
+    #     order_price_difference=25,
+    #     start_price=4500,
+    #     max_order_count=20,
+    #     post_only=True
+    # )
+    # print(f"Count: {len(orders)}")
+    # print(orders[-1])
 
 
     ##################### SPOT
