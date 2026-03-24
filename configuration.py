6853dadb6000caa890a1c738382abe1b9da02bc0
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-24 15:51:04 -0400

    cc

--- configuration.py
+++ configuration.py
@@ -160,8 +160,8 @@ class OrderBook():
             "SELL": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 2
         },
         "FUTURE": {
-            "BUY": 0.01,
-            "SELL": 0.01
+            "BUY": 0.02,
+            "SELL": 0.02
         }
     }
 
