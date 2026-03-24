7f7b39c8d5a35afa4eb7c9e1d2b10676c56c5c89
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-03-24 10:29:45 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -4,18 +4,18 @@ from order import create_limit_order_span
 if __name__ == "__main__":
     ##################### PERP
     # BTC
-    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
-    #     delay_in_secs=0,
-    #     product_id="BIP-20DEC30-CDE",
-    #     side="SELL",
-    #     order_base_size_range={"start": 10, "stop": 20},
-    #     order_price_difference=400,
-    #     start_price=71200,
-    #     max_order_count=10,
-    #     post_only=True
-    # )
-    # print(f"Count: {len(orders)}")
-    # print(orders[-1])
+    orders = create_limit_order_span( #size=2, diff=25, count=100, 
+        delay_in_secs=0,
+        product_id="BIP-20DEC30-CDE",
+        side="SELL",
+        order_base_size_range={"start": 5, "stop": 10},
+        order_price_difference=105,
+        start_price=70325,
+        max_order_count=20,
+        post_only=True
+    )
+    print(f"Count: {len(orders)}")
+    print(orders[-1])
 
     # orders = create_limit_order_span( #size=2, diff=25, count=100, 
     #     delay_in_secs=0,
@@ -31,18 +31,18 @@ if __name__ == "__main__":
     # print(orders[-1])
 
     # PAXG
-    orders = create_limit_order_span(
-        delay_in_secs=0,
-        product_id="PAU-20DEC30-CDE",
-        side="BUY",
-        order_base_size_range={"start": 2, "stop": 5},
-        order_price_difference=35,
-        start_price=4350,
-        max_order_count=10,
-        post_only=True
-    )
-    print(f"Count: {len(orders)}")
-    print(orders[-1])
+    # orders = create_limit_order_span(
+    #     delay_in_secs=0,
+    #     product_id="PAU-20DEC30-CDE",
+    #     side="BUY",
+    #     order_base_size_range={"start": 2, "stop": 5},
+    #     order_price_difference=35,
+    #     start_price=4350,
+    #     max_order_count=10,
+    #     post_only=True
+    # )
+    # print(f"Count: {len(orders)}")
+    # print(orders[-1])
 
 
     ##################### SPOT
