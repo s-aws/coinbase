4bed5cb991160de9d3c2044c540e0bd9153514f8
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-04-02 01:36:04 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     orders = create_limit_order_span( #size=2, diff=25, count=100, 
         delay_in_secs=0,
         product_id="BIP-20DEC30-CDE",
-        side="SELL",
+        side="BUY",
         order_base_size_range={"start": 1, "stop": 2},
         order_price_difference=40,
         start_price=66170,
