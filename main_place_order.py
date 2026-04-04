70c28475bf460d436dc7b6f17367515145f03f57
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-04-04 18:34:35 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -33,18 +33,18 @@ if __name__ == "__main__":
 
     # ETH
 
-    orders = create_limit_order_span( #size=5, diff=1 (0.05%), count=20, 
-        delay_in_secs=0,
-        product_id="ETP-20DEC30-CDE",
-        side="SELL",
-        order_base_size_range={"start": 1, "stop": 1},
-        order_price_difference=2,
-        start_price=2094.5,
-        max_order_count=1,
-        post_only=True
-    )
-    print(f"Count: {len(orders)}")
-    print(orders[-1])
+    # orders = create_limit_order_span( #size=5, diff=1 (0.05%), count=20, 
+    #     delay_in_secs=0,
+    #     product_id="ETP-20DEC30-CDE",
+    #     side="SELL",
+    #     order_base_size_range={"start": 1, "stop": 3},
+    #     order_price_difference=1,
+    #     start_price=2072.5,
+    #     max_order_count=100,
+    #     post_only=True
+    # )
+    # print(f"Count: {len(orders)}")
+    # print(orders[-1])
 
     # PAXG
     # orders = create_limit_order_span( #size=1, diff=4.5 (0.1%), count=10,
@@ -63,17 +63,17 @@ if __name__ == "__main__":
 
     ##################### SPOT
 
-    # orders = create_limit_order_span(
-    #     delay_in_secs=1,
-    #     product_id="BTC-USDC",
-    #     side="BUY",
-    #     order_base_size_range={"start": 0.001, "stop": 0.005},
-    #     order_price_difference=10,
-    #     start_price=67450.33,
-    #     max_order_count=200,
-    #     post_only=True
-    # )
+    orders = create_limit_order_span(
+        delay_in_secs=1,
+        product_id="BTC-USDC",
+        side="SELL",
+        order_base_size_range={"start": 0.0001, "stop": 0.0005},
+        order_price_difference=5,
+        start_price=67300.99,
+        max_order_count=200,
+        post_only=True
+    )
 
-    # print(f"Count: {len(orders)}")
-    # print(orders[-1])
+    print(f"Count: {len(orders)}")
+    print(orders[-1])
 
