3427ce570bf6074f6d079297970070f83788b8de
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-04-04 10:15:26 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -17,34 +17,34 @@ if __name__ == "__main__":
     # print(f"Count: {len(orders)}")
     # print(orders[-1])
 
-    orders = create_limit_order_span( #size=2, diff=25, count=100, 
-        delay_in_secs=0,
-        product_id="BIP-20DEC30-CDE",
-        side="BUY",
-        order_base_size_range={"start": 1, "stop": 1},
-        order_price_difference=25,
-        start_price=66600,
-        max_order_count=110,
-        post_only=True
-    )
-    print(f"Count: {len(orders)}")
-    print(orders[-1])
-
-
-    # ETH
-
-    # orders = create_limit_order_span( #size=5, diff=1 (0.05%), count=20, 
+    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
     #     delay_in_secs=0,
-    #     product_id="ETP-20DEC30-CDE",
+    #     product_id="BIP-20DEC30-CDE",
     #     side="BUY",
-    #     order_base_size_range={"start": 10, "stop": 10},
-    #     order_price_difference=2,
-    #     start_price=2000,
-    #     max_order_count=20,
+    #     order_base_size_range={"start": 1, "stop": 1},
+    #     order_price_difference=25,
+    #     start_price=66600,
+    #     max_order_count=110,
     #     post_only=True
     # )
     # print(f"Count: {len(orders)}")
     # print(orders[-1])
+
+
+    # ETH
+
+    orders = create_limit_order_span( #size=5, diff=1 (0.05%), count=20, 
+        delay_in_secs=0,
+        product_id="ETP-20DEC30-CDE",
+        side="SELL",
+        order_base_size_range={"start": 2, "stop": 4},
+        order_price_difference=2,
+        start_price=2084.5,
+        max_order_count=1,
+        post_only=True
+    )
+    print(f"Count: {len(orders)}")
+    print(orders[-1])
 
     # PAXG
     # orders = create_limit_order_span( #size=1, diff=4.5 (0.1%), count=10,
