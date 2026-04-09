b69df921a73f4539e68ddeb9cb98809ff16588d6
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-04-09 09:01:06 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -2,34 +2,9 @@
 from order import create_limit_order_span
 
 if __name__ == "__main__":
-    ##################### PERP
+    ##################### PERP 
+
     # BTC
-    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
-    #     delay_in_secs=0,
-    #     product_id="BIT-24APR26-CDE",
-    #     side="SELL",
-    #     order_base_size_range={"start": 10, "stop": 10},
-    #     order_price_difference=150,
-    #     start_price=68375,
-    #     max_order_count=10,
-    #     post_only=True
-    # )
-    # print(f"Count: {len(orders)}")
-    # print(orders[-1])
-
-    orders = create_limit_order_span( #size=2, diff=25, count=100, 
-        delay_in_secs=0,
-        product_id="BIT-24APR26-CDE",
-        side="SELL",
-        order_base_size_range={"start": 1, "stop": 5},
-        order_price_difference=100,
-        start_price=72500,
-        max_order_count=40,
-        post_only=True
-    )
-    print(f"Count: {len(orders)}")
-    print(orders[-1])
-
     # orders = create_limit_order_span( #size=2, diff=25, count=100, 
     #     delay_in_secs=0,
     #     product_id="BIP-20DEC30-CDE",
@@ -43,22 +18,7 @@ if __name__ == "__main__":
     # print(f"Count: {len(orders)}")
     # print(orders[-1])
 
-    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
-    #     delay_in_secs=0,
-    #     product_id="BIP-20DEC30-CDE",
-    #     side="BUY",
-    #     order_base_size_range={"start": 1, "stop": 1},
-    #     order_price_difference=25,
-    #     start_price=66600,
-    #     max_order_count=110,
-    #     post_only=True
-    # )
-    # print(f"Count: {len(orders)}")
-    # print(orders[-1])
-
-
     # ETH
-
     # orders = create_limit_order_span( #size=5, diff=1 (0.05%), count=20, 
     #     delay_in_secs=0,
     #     product_id="ETP-20DEC30-CDE",
@@ -86,9 +46,25 @@ if __name__ == "__main__":
     # print(f"Count: {len(orders)}")
     # print(orders[-1])
 
+    ##################### 24 APR 26 #####################
 
-    ##################### SPOT
+    # BTC
+    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
+    #     delay_in_secs=0,
+    #     product_id="BIT-24APR26-CDE",
+    #     side="SELL",
+    #     order_base_size_range={"start": 1, "stop": 5},
+    #     order_price_difference=100,
+    #     start_price=72500,
+    #     max_order_count=40,
+    #     post_only=True
+    # )
+    # print(f"Count: {len(orders)}")
+    # print(orders[-1])
 
+    ##################### SPOT #####################
+
+    # BTC
     # orders = create_limit_order_span(
     #     delay_in_secs=1,
     #     product_id="BTC-USDC",
