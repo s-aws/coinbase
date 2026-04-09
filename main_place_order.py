3a6a22f8ec5c7d7dc493931585786a3c3a18cd0f
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-04-09 09:10:36 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -5,18 +5,18 @@ if __name__ == "__main__":
     ##################### PERP 
 
     # BTC
-    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
-    #     delay_in_secs=0,
-    #     product_id="BIP-20DEC30-CDE",
-    #     side="SELL",
-    #     order_base_size_range={"start": 10, "stop": 10},
-    #     order_price_difference=150,
-    #     start_price=67050,
-    #     max_order_count=9,
-    #     post_only=True
-    # )
-    # print(f"Count: {len(orders)}")
-    # print(orders[-1])
+    orders = create_limit_order_span( #size=2, diff=25, count=100, 
+        delay_in_secs=0,
+        product_id="BIP-20DEC30-CDE",
+        side="BUY",
+        order_base_size_range={"start": 5, "stop": 10},
+        order_price_difference=100,
+        start_price=71000,
+        max_order_count=10,
+        post_only=True
+    )
+    print(f"Count: {len(orders)}")
+    print(orders[-1])
 
     # ETH
     # orders = create_limit_order_span( #size=5, diff=1 (0.05%), count=20, 
