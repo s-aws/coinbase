1075d0028de79fedd6231de0960a6f8722e4aee3
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-04-09 17:41:03 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -5,18 +5,18 @@ if __name__ == "__main__":
     ##################### PERP 
 
     # BTC
-    orders = create_limit_order_span( #size=2, diff=25, count=100, 
-        delay_in_secs=0,
-        product_id="BIP-20DEC30-CDE",
-        side="BUY",
-        order_base_size_range={"start": 5, "stop": 10},
-        order_price_difference=100,
-        start_price=71000,
-        max_order_count=10,
-        post_only=True
-    )
-    print(f"Count: {len(orders)}")
-    print(orders[-1])
+    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
+    #     delay_in_secs=0,
+    #     product_id="BIP-20DEC30-CDE",
+    #     side="BUY",
+    #     order_base_size_range={"start": 1, "stop": 5},
+    #     order_price_difference=135,
+    #     start_price=71000,
+    #     max_order_count=20,
+    #     post_only=True
+    # )
+    # print(f"Count: {len(orders)}")
+    # print(orders[-1])
 
     # ETH
     # orders = create_limit_order_span( #size=5, diff=1 (0.05%), count=20, 
@@ -49,18 +49,18 @@ if __name__ == "__main__":
     ##################### 24 APR 26 #####################
 
     # BTC
-    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
-    #     delay_in_secs=0,
-    #     product_id="BIT-24APR26-CDE",
-    #     side="SELL",
-    #     order_base_size_range={"start": 1, "stop": 5},
-    #     order_price_difference=100,
-    #     start_price=72500,
-    #     max_order_count=40,
-    #     post_only=True
-    # )
-    # print(f"Count: {len(orders)}")
-    # print(orders[-1])
+    orders = create_limit_order_span( #size=2, diff=25, count=100, 
+        delay_in_secs=0,
+        product_id="BIT-24APR26-CDE",
+        side="SELL",
+        order_base_size_range={"start": 10, "stop": 10},
+        order_price_difference=100,
+        start_price=72570,
+        max_order_count=10,
+        post_only=True
+    )
+    print(f"Count: {len(orders)}")
+    print(orders[-1])
 
     ##################### SPOT #####################
 
