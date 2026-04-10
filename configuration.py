19c0ab5ce0c901ffc5cab5eaa9dab67a9a41b063
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-04-10 18:51:21 -0400

    cc

--- configuration.py
+++ configuration.py
@@ -177,8 +177,8 @@ class OrderBook():
             "SELL": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 4
         },
         "FUTURE": {
-            "BUY": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 8,
-            "SELL": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 8
+            "BUY": 0.014,
+            "SELL": 0.014
         },
         "BIP-20DEC30-CDE": {
             "BUY": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 14,
