e29d40d88894846446fbd8dbd1c7e15099758695
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-04-11 08:23:09 -0400

    cc

--- main_place_order.py
+++ main_place_order.py
@@ -7,18 +7,18 @@ if __name__ == "__main__":
 
     # BTC
 
-    orders = create_limit_order_span( #size=2, diff=25, count=100, 
-        delay_in_secs=0,
-        product_id="BIP-20DEC30-CDE",
-        side="BUY",
-        order_base_size_range={"start": 1, "stop": 2},
-        order_price_difference=35,
-        start_price=72875,
-        max_order_count=100,
-        post_only=True
-    )
-    print(json.dumps(orders))
-    print(f"Count: {len(orders)}")
+    # orders = create_limit_order_span( #size=2, diff=25, count=100, 
+    #     delay_in_secs=0,
+    #     product_id="BIP-20DEC30-CDE",
+    #     side="BUY",
+    #     order_base_size_range={"start": 1, "stop": 2},
+    #     order_price_difference=35,
+    #     start_price=72875,
+    #     max_order_count=100,
+    #     post_only=True
+    # )
+    # print(json.dumps(orders))
+    # print(f"Count: {len(orders)}")
 
     # ETH
     # orders = create_limit_order_span( #size=5, diff=1 (0.05%), count=20, 
@@ -50,7 +50,7 @@ if __name__ == "__main__":
 
     ##################### 24 APR 26 #####################
 
-    BTC
+    # BTC
     orders = create_limit_order_span( #size=2, diff=25, count=100, 
         delay_in_secs=0,
         product_id="BIT-24APR26-CDE",
