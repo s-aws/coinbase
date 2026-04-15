f32c8353b88c5bee0620bf89a0e035a4f3025a5f
SH Groom <95631595+newscorp-sgroom@users.noreply.github.com>
2026-04-15 10:10:51 -0400

    cc

--- main.py
+++ main.py
@@ -92,10 +92,14 @@ def process_user_event(event):
             return
 
         if "orders" in event and event["type"].upper() in ["OPEN", "FILLED", "CANCELLED", "UPDATE"]:
+            processed = []
             for order in event["orders"]:
                 if "client_order_id" not in order:
                     print(f"Missing client_order_id in order event: {order}")
                     continue
+                if order["client_order_id"] in processed:
+                    continue
+                processed.append(order["client_order_id"])
                 process_user_order(order)
 
         if "positions" in event:
