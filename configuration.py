e706b8f6c1a88f1d64590024c04200c77aad5462
SH Groom <the.count@s-aws.com>
2026-04-20 02:51:52 -0400

    cc

--- configuration.py
+++ configuration.py
@@ -705,16 +705,16 @@ class OrderBook():
 
     profit = {
         "SPOT": {
-            "BUY": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 8,
-            "SELL": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 8
+            "BUY": 0.004,
+            "SELL": 0.004
         },
         "FUTURE": {
             "BUY": 0.004,
             "SELL": 0.004
         },
         "BIP-20DEC30-CDE": {
-            "BUY": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 28,
-            "SELL": float(transaction_summary["fee_tier"]["taker_fee_rate"]) * 28
+            "BUY": 0.004,
+            "SELL": 0.004
         }
     }
 
